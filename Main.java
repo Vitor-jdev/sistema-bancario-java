ed3d15b68910a1e82e606cdb227110b68ccb8cc4
Vitor-jdev <Vitorcidadegomes23@gmail.com>
2026-03-21 23:26:53 -0300

    Implement basic banking application in Main.java

---
+++ Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        
+        double saldo = 1000;
+         
+        System.out.println("seu saldo e: " + saldo);
+
+        while (true) {
+
+            System.out.println("1 - Ver saldo");
+            System.out.println("2 - Depositar");
+            System.out.println("3 - Sacar");
+            System.out.println("4 - Sair");
+
+            int opcao = teclado.nextInt();
+
+            if (opcao == 1) {
+                System.out.println("seu saldo e: " + saldo);               
+            } else if (opcao == 2) {
+                System.out.println("digite o valor para depositar:");
+                double valor = teclado.nextDouble();
+                saldo = saldo + valor;
+                System.out.println("Deposito realizado!");
+            }  else if (opcao == 3) {
+    System.out.println("Digite o valor para sacar:");
+    double valor = teclado.nextDouble();
+
+    if (valor <= saldo) {
+        saldo = saldo - valor;
+        System.out.println("Saque realizado!");
+    } else {
+        System.out.println("Saldo insuficiente!");
+    }
+               } else if (opcao == 4) {
+                System.out.println("saindo...");
+                break;
+               }     
+        }   
+    }
+}
